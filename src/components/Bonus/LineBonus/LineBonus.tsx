99d133ee2d4e4c445b9afc0a09081ef6fda1dda7
Davi-Quirino <davi.quirino@ab-inbev.com>
2023-11-27 20:48:12 -0300

    aos

--- src/components/Bonus/LineBonus/LineBonus.tsx
+++ src/components/Bonus/LineBonus/LineBonus.tsx
@@ -1,29 +1,41 @@
+import AOS from "aos";
+import { useEffect } from "react";
 interface LineBonusProps {
-    img: string;
-    title: string;
-    subtitle: string;
-    orientation: 'left' | 'right';
+  img: string;
+  title: string;
+  subtitle: string;
+  orientation: "left" | "right";
 }
-export function LineBonus({img, title, subtitle, orientation}: LineBonusProps){
-    return (
-        <div className="line-bonus">
-            {orientation === 'left' ? (
-                <div className="line-bonus__content">
-                    <img src={img} alt="Bonus Code Start"/>
-                    <div className="line-bonus_content-texts">
-                        <h1>{title}</h1>
-                        <p>{subtitle}</p>
-                    </div>
-                </div>
-            ) : (
-                <div className="line-bonus__content">
-                    <div className="line-bonus_content-texts">
-                        <h1>{title}</h1>
-                        <p>{subtitle}</p>
-                    </div>
-                    <img src={img} alt="Bonus Code Start"/>
-                </div>
-            )}
+export function LineBonus({
+  img,
+  title,
+  subtitle,
+  orientation,
+}: LineBonusProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+  return (
+    <div className="line-bonus" data-aos="fade-up">
+      {orientation === "left" ? (
+        <div className="line-bonus__content">
+          <img src={img} alt="Bonus Code Start" />
+          <div className="line-bonus_content-texts">
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+          </div>
         </div>
-    )
+      ) : (
+        <div className="line-bonus__content">
+          <div className="line-bonus_content-texts">
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+          </div>
+          <img src={img} alt="Bonus Code Start" />
+        </div>
+      )}
+    </div>
+  );
 }
