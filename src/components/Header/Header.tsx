0425c702f9ab039c3c461fa3290e455ca1b16b36
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-11-27 20:24:21 -0300

    refactor header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,22 +5,25 @@ export function Header() {
     <div className="header">
       <div className="container">
         <div className="header__content">
-          <div className="header__content-logo">
-            <img src="/logo-code-start.svg" alt="logo" />
+          <div className="header__content-container">
+            <div className="header__content-logo">
+              <img src="/logo-code-start.svg" alt="logo" />
+              <button className="btn-buy">Quero trabalhar de casa</button>
+            </div>
           </div>
           <div className="header__content-titles">
             <h1 className="header__content-titles-title">
               Ganhe acima da média com programação
             </h1>
             <h2 className="header__content-titles-subtitle">
-              Existem mais de <span>
-                15.000 vagas disponíveis hoje
-              </span>{" "}
-              e uma delas pode ser sua,{" "}
+              Existem mais de <span>15.000 vagas disponíveis hoje</span> e uma
+              delas pode ser sua,{" "}
               <span>mesmo que você não saiba nada de programação!</span>
             </h2>
           </div>
-          <button className="btn-buy">Quero trabalhar de casa</button>
+          <button className="btn-buy" style={{ marginTop: "2em" }}>
+            Quero trabalhar de casa
+          </button>
         </div>
       </div>
     </div>
