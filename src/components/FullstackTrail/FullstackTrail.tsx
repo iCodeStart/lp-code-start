d62a32511e206cb0defeb0f4cf2acae4c9951144
Alan Braulio <alan-braulio@hotmail.com>
2023-11-29 22:44:49 -0300

    centered tag

--- src/components/FullstackTrail/FullstackTrail.tsx
+++ src/components/FullstackTrail/FullstackTrail.tsx
@@ -1,20 +1,28 @@
 import { Chip } from "../Chip";
 
-export function FullstackTrail(){
-    return (
-        <div className="fullstack-trail">
-            <div className="container">
-                <Chip label="Trilha" />
-                <h1 className="section-title">Nossa trilha fullstack</h1>
-                <div className="fullstack-trail__content">
-                    <div className="fullstack-desktop">
-                        <img src="/our-trail-fullstack.png" alt="Trilha fullstack Code Start"/>
-                    </div>
-                    <div className="fullstack-mobile">
-                        <img src="/our-trail-fullstack-mobile.png" alt="Trilha fullstack Code Start"/>
-                    </div>
-                </div>
-            </div>
+export function FullstackTrail() {
+  return (
+    <div className="fullstack-trail">
+      <div className="container">
+        <div className="title-container">
+          <Chip label="Trilha" />
+          <h1 className="section-title">Nossa trilha fullstack</h1>
         </div>
-    )
+        <div className="fullstack-trail__content">
+          <div className="fullstack-desktop">
+            <img
+              src="/our-trail-fullstack.png"
+              alt="Trilha fullstack Code Start"
+            />
+          </div>
+          <div className="fullstack-mobile">
+            <img
+              src="/our-trail-fullstack-mobile.png"
+              alt="Trilha fullstack Code Start"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
