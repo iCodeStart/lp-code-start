c58fa708471dcaf51dce5edf163b208c0324c985
Alan Braulio <alan-braulio@hotmail.com>
2023-11-29 23:10:32 -0300

    removing effects cards

--- src/components/Projects/CardInfoProject/CardInfoProject.tsx
+++ src/components/Projects/CardInfoProject/CardInfoProject.tsx
@@ -1,16 +1,9 @@
-import AOS from "aos";
-import { useEffect } from "react";
 interface CardInfoProjectProps {
   text: string;
 }
 export function CardInfoProject({ text }: CardInfoProjectProps) {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-    });
-  }, []);
   return (
-    <div className="card-info-project" data-aos="fade-left">
+    <div className="card-info-project">
       <img src="./icon-benefits.svg" alt="Benefícios Code Start" />
       <p>{text}</p>
     </div>
