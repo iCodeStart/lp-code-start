44d00a2348ab804e826b5498ffe6a32eb16bc1f2
Alan Braulio <alan-braulio@hotmail.com>
2023-11-29 23:08:41 -0300

    removing card-info-project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { CardInfoProject } from "./CardInfoProject";
 import { Chip } from "../Chip";
 
 export function Projects() {
@@ -70,9 +69,6 @@ export function Projects() {
             />
           </div>
           <h3>E muito mais....</h3>
-          <CardInfoProject text="Todo mês desenvolvemos juntos muitos exercícios com resolução, projetos e desafios!" />
-          <CardInfoProject text="Ao finalizar, você terá um repertório de respeito e experiência para desenvolver soluções alinhadas com diversas áreas do mercado de trabalho" />
-          <CardInfoProject text="Certificado reconhecido pelas empresas" />
         </div>
       </div>
     </div>
