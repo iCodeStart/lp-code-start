5529acb524ab85072a9b6849a02100f2ae1fd930
Alan Braulio <alan-braulio@hotmail.com>
2024-03-18 23:02:38 -0300

    removing unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppWrapper from './AppWrapper';
-import { Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
