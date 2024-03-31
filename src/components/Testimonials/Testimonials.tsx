5090e455c11898e9809ddd67fa3155b31c6fde7b
Alan Braulio <alanbraulio@ciandt.com>
2024-03-31 15:41:07 -0300

    add testimonials slider

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,14 +1,29 @@
-import AOS from "aos";
-import { useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Chip } from "../Chip";
 import "./Testimonials.styles.scss";
 
 export function Testimonials() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-    });
-  }, []);
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+
+  const imagesTestimonials = [
+    "./depoimento-6.jpeg",
+    "./depoimento-5.jpeg",
+    "./depoimento-4.jpeg",
+    "./depoimento-1.jpeg",
+    "./depoimento-2.jpeg",
+    "./depoimento-3.jpeg",
+  ];
   return (
     <div className="testimonials">
       <div className="container">
@@ -18,37 +33,20 @@ export function Testimonials() {
             Veja o que nossos alunos acham da Code Start!
           </h1>
         </div>
-        <div className="testimonials__content">
-          <img
-            src="./depoimento-6.jpeg"
-            alt="Depoimento Code Start"
-            data-aos="fade-left"
-          />
-          <img
-            src="./depoimento-5.jpeg"
-            alt="Depoimento Code Start"
-            data-aos="fade-up"
-          />
-          <img
-            src="./depoimento-4.jpeg"
-            alt="Depoimento Code Start"
-            data-aos="fade-right"
-          />
-          <img
-            src="./depoimento-1.jpeg"
-            alt="Depoimento Code Start"
-            data-aos="fade-right"
-          />
-          <img
-            src="./depoimento-2.jpeg"
-            alt="Depoimento Code Start"
-            data-aos="fade-up"
-          />
-          <img
-            src="./depoimento-3.jpeg"
-            alt="Depoimento Code Start"
-            data-aos="fade-left"
-          />
+        <div className="slider-container">
+          <Slider {...settings}>
+            {imagesTestimonials.map((item) => {
+              return (
+                <div>
+                  <img
+                    src={item}
+                    alt="img"
+                    className="slide-image-carousel-testimonial-items"
+                  />
+                </div>
+              );
+            })}
+          </Slider>
         </div>
       </div>
     </div>
