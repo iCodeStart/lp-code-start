fc82f168f767c753a4e3d7f325536b0670ef5720
Davi-Quirino <davi.quirino@ab-inbev.com>
2024-03-31 20:56:23 -0300

    improving page

--- src/components/LPProjects/Guarantee/Guarantee.tsx
+++ src/components/LPProjects/Guarantee/Guarantee.tsx
@@ -3,12 +3,29 @@ import "./Guarantee.styles.scss";
 export function Guarantee() {
   return (
     <div className="content-guarantee">
-      <img
-        className="image"
-        src="./promessa.jpg"
-        alt="promise"
-        style={{ width: "100%" }}
-      />
+      <div className="content-text">
+        A nossa promessa é forte, mas é totalmente baseada nos resultados desse
+        método nos nossos alunos.
+      </div>
+      <div className="content-text">
+        É bem simples. Se você fizer todo o treinamento dentro de 1 ano e não
+        conseguir a sua vaga de emprego, vamos te dar uma mentoria individual
+        para ver onde você precisa melhorar.
+      </div>
+      <div className="content-text">
+        Mas se mesmo assim, você ainda nao conseguir o seu objetivo, nada mais
+        justo que devolvermos 100% do que você investiu.
+      </div>
+
+      <div className="content-text">
+        Resumindo, você tem a garantia de que se fizer o treinamento completo
+        dentro de 1 ano e não conseguir o seu resultado, terá todo o seu
+        investimento devolvido.
+      </div>
+
+      <div className="content-text">
+        Me responde com a maior sinceridade que puder. O que você tem a perder?
+      </div>
     </div>
   );
 }
