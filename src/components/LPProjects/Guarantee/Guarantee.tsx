b2f7208e21b832ba804f90c7be8d16f6b40f22bb
Davi-Quirino <davi.quirino@ab-inbev.com>
2024-07-20 13:10:02 -0300

    removing png photos

--- src/components/LPProjects/Guarantee/Guarantee.tsx
+++ src/components/LPProjects/Guarantee/Guarantee.tsx
@@ -14,7 +14,11 @@ export function Guarantee() {
       <div className="retangulo">
         <h1 style={{ color: "#D8D8D8" }}>
           NOSSA PROPOSTA: <br />
-          <img className="guarantee-img" src="https://i.imgur.com/r29LD5Y.png" alt="Code Start"/>
+          <img
+            className="guarantee-img"
+            src="https://i.imgur.com/r29LD5Y.png"
+            alt="Code Start"
+          />
           <br />
           <span style={{ color: "#fff" }}>UMA GARANTIA DE</span>{" "}
           <span style={{ color: "#00bcd3" }}>365 DIAS</span>
@@ -31,40 +35,6 @@ export function Guarantee() {
           com certeza, ou nós perdemos dinheiro.
         </p>
       </div>
-      {/* <div className="content-guarantee">
-        <div>
-          <img src="./jobOffer1.png" alt="method" className="image" />
-        </div>
-        <div className="content-context">
-          <h1
-            style={{ color: "white", textAlign: "center", lineHeight: "50px" }}
-          >
-            A responsabilidade é toda nossa!
-          </h1>
-
-          <br />
-          <div className="content-text">
-            Se dentro de 1 ano você não conseguir o seu resultado, basta
-            preencher um formulário e nós{" "}
-            <span style={{ color: "white" }}>
-              devolveremos 100% do seu investimento.
-            </span>
-          </div>
-          <br />
-          <br />
-          <div>
-            Você tem tudo para conquistar a vaga dos seus sonhos seguindo o
-            nosso método.
-          </div>
-          <div>
-            {" "}
-            <br />
-            <br />
-            Aqui você terá um suporte único e diferenciado para que você passe
-            com certeza, ou nós perdemos dinheiro.
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
