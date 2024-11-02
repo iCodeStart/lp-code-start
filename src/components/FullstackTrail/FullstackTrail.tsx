c84b217e8c6ea11cc9ed4b54c0f33f9e5be92164
Davi-Quirino <davi.quirino@ab-inbev.com>
2024-11-02 15:37:16 -0300

    improving

--- src/components/FullstackTrail/FullstackTrail.tsx
+++ src/components/FullstackTrail/FullstackTrail.tsx
@@ -10,17 +10,11 @@ export function FullstackTrail() {
         </div>
         <div className="fullstack-trail__content">
           <div className="fullstack-desktop">
-            <img
-              src="/our-trail-fullstack.webp"
-              alt="Trilha fullstack Code Start"
-            />
+            <img src="/roadmapCode.webp" alt="Trilha fullstack Code Start" />
             {/* <img src="/roadmapCodedesk.png" alt="Trilha fullstack Code Start" /> */}
           </div>
           <div className="fullstack-mobile">
-            <img
-              src="/our-trail-fullstack-mobile.webp"
-              alt="Trilha fullstack Code Start"
-            />
+            <img src="/roadmapCode.webp" alt="Trilha fullstack Code Start" />
             {/* <img src="/roadmapCode.png" alt="Trilha fullstack Code Start" /> */}
           </div>
         </div>
