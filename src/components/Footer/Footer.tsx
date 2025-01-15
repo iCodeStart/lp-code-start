ea38aae052e9f5102b3ce9655f874a1fdaf5e16b
Davi-Quirino <davi.quirino@ab-inbev.com>
2025-01-15 11:40:27 -0300

    trocando footer para 2025

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
         <div className="footer__content">
           <img src="./logo-code-start.svg" alt="Logo Code Start" />
           <div className="copyright">
-            <span>Code Start © 2024</span>
+            <span>Code Start © 2025</span>
             <span>Todos os direitos reservados</span>
           </div>
         </div>
