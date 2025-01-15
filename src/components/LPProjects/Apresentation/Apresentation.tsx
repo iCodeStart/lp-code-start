ab2a0fe978de608539080caad1bde2c59551f9b6
Davi-Quirino <davi.quirino@ab-inbev.com>
2025-01-15 11:36:10 -0300

    Tirando a foto dos professores e colocando a logo

--- src/components/LPProjects/Apresentation/Apresentation.tsx
+++ src/components/LPProjects/Apresentation/Apresentation.tsx
@@ -1,29 +1,47 @@
 import "./Apresentation.styles.scss";
+//import logo from '../../../../public/logo-code-start.svg'
 
 export function Apresentation() {
   return (
     <div className="content-lp-project-apresentation">
-      <div className="overlay"></div>
-      <div className="content-lp-project-apresentation-container">
-        <span className="content-lp-project-apresentation-product-name">
+      <>
+        <div className="overlay"></div>
+
+        <div className="content-lp-project-apresentation-container">
+          {/* <span className="content-lp-project-apresentation-product-name">
           MÉTODO AAP
-        </span>
-        <div className="content-lp-project-apresentation-box-texts">
-          <h1>
-            Você nunca mais vai pensar em desistir e se sentir perdido na
-            programação
-          </h1>
-          <p>
-            Domine a programação com o método <strong>AAP</strong> e conquiste a
-            sua vaga de emprego de forma acelerada - Estudando apenas 1 hora por
-            dia!
-          </p>
-          <p>
-            Aqui nós <strong style={{ color: "white" }}>GARANTIMOS</strong> a
-            sua aprovação.
-          </p>
+        </span> */}
+          <span className="content-lp-project-apresentation-product-name">
+            <img
+              style={{ width: "20rem" }}
+              src="/logo-code-start.svg"
+              alt="logo"
+            />
+            <span className="content-lp-project-apresentation-alert">
+              <i
+                className="fas fa-exclamation-circle"
+                style={{ marginRight: "0.5rem", color: "#00bcd3" }}
+              />
+              A MELHOR OPORTUNIDADE DE 2025!
+            </span>
+          </span>
+          <div className="content-lp-project-apresentation-box-texts">
+            <h1>
+              Você nunca mais vai pensar em desistir e se sentir perdido na
+              programação
+            </h1>
+            <p>
+              Domine a programação com o método <strong>AAP</strong> e conquiste
+              a sua vaga de emprego de forma acelerada - Estudando apenas 1 hora
+              por dia!
+            </p>
+            <p>
+              Aqui nós <strong style={{ color: "white" }}>GARANTIMOS</strong> a
+              sua aprovação.
+            </p>
+          </div>
         </div>
-      </div>
+      </>
     </div>
   );
 }
