64a744de4b574bae6e6646a1025ec09d6a7cf52c
Davi-Quirino <davi.quirino@ab-inbev.com>
2025-04-22 09:43:06 -0300

    change number contact

--- src/components/Whatsapp/Whatsapp.tsx
+++ src/components/Whatsapp/Whatsapp.tsx
@@ -1,16 +1,24 @@
-import './Whatsapp.styles.scss'
+import "./Whatsapp.styles.scss";
 
 interface WhatsappProps {
-  customMessage?: string
+  customMessage?: string;
 }
 export function Whatsapp({ customMessage }: WhatsappProps) {
   const defaultMessage = encodeURIComponent(
     "Olá, gostaria de saber mais sobre o Treinamento da Code Start!"
   );
- 
+
   return (
     <div className="whatsapp-icon">
-    <a href={`https://api.whatsapp.com/send?phone=+5531997506754&text=${customMessage || defaultMessage}`} target="_blank" rel="noreferrer"><img className="whatsapp-image" src="/whatsapp1.webp" alt="WhatsApp" /></a>
+      <a
+        href={`https://api.whatsapp.com/send?phone=+5532998573527&text=${
+          customMessage || defaultMessage
+        }`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="whatsapp-image" src="/whatsapp1.webp" alt="WhatsApp" />
+      </a>
     </div>
   );
 }
