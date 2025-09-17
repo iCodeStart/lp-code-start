05faa09621a5c7c3fd08fd4abfc5132ee17308b6
alanbraulio <alan-braulio@hotmail.com>
2025-09-16 21:30:41 -0300

    removing styles

--- src/pages/ThankYou/ThankYou.tsx
+++ src/pages/ThankYou/ThankYou.tsx
@@ -2,7 +2,7 @@ import './ThankYou.styles.scss'; // Atualizar caminho se necessário
 
 const ThankYou = () => {
   return (
-    <div className='container'>
+    <div className='container-thankyou'>
       <h1 className='title-thank-you'>Obrigado pela sua compra!</h1>
       <p className='text-thank-you'>Agradecemos por escolher nosso produto. Esperamos que atenda suas expectativas!</p>
       <p className='text-thank-you'>Acesse o seu dashboard da Hotmart clicando no botão abaixo</p>
